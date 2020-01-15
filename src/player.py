548f326cede06cf8d501205d8779add0adfbb632
Diana Grams <deegrams221@gmail.com>
2020-01-15 16:41:14 -0700

    added inventory and add_items to 'player.py', fixed the attributes in 'room.py'

--- src/player.py
+++ src/player.py
@@ -4,10 +4,13 @@ class Player:
     def __init__(self, name, room):
         self.name = name
         self.room = room
+        self.inventory = []
     def __str__(self):
         return f"Player: {self.name}"
     def __repr__(self):
         return f"Player({repr(self.name, self.room)})"
+    def add_items(self, item):
+        self.inventory.append(item)
 
 # Day 2:
 # Add capability to add `Item`s to the player's inventory. The inventory can
