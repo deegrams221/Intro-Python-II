dc7f8a9b9861ce6d1e0744c4cb6770117a8627a9
Diana Grams <deegrams221@gmail.com>
2020-01-15 17:02:40 -0700

    added 'from item import Item' to 'player.py' and 'room.py'

--- src/player.py
+++ src/player.py
@@ -1,5 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from item import Item
+
 class Player:
     def __init__(self, name, room):
         self.name = name
